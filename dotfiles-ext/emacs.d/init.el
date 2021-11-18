e00e7462a9b281507e9a32fa46e693431cd88a81
suprhst <79753461+suprhst@users.noreply.github.com>
2021-11-18 06:18:21 +0530

    Added Org Roam cfg and some misc stuff

--- dotfiles-ext/emacs.d/init.el
+++ dotfiles-ext/emacs.d/init.el
@@ -48,7 +48,7 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(awesome-tray-mode-line-active-color "#2fafff")
  '(awesome-tray-mode-line-inactive-color "#323232")
- '(custom-enabled-themes '(tria))
+ '(custom-enabled-themes '(exotica))
  '(custom-safe-themes
    '("9f71346859d9759899ce45a9543cdf47f7af76219b7e398da0256b58b34e9c17" "c7f259e7b3d4591a7eea5aac11790b16037756220bffa908f96cf7e85e99b58d" "4535dd36f4c6d89add105f545f1cb6f63500cd76e65b6ba563e080bc30670746" "3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" "b7133876a11eb2ded01b4b144b45d9e7457f80dd5900c332241881ab261c50f4" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "7af77cabcacee94c4920637352a342c70e1e528345c5de6ed6c912e713dd8973" "6da0430b5e97ca069858009beaae0ca27e38c37a55fda2cb0af97e083990af86" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "31f1723fb10ec4b4d2d79b65bcad0a19e03270fe290a3fc4b95886f18e79ac2f" "57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" default))
  '(exwm-floating-border-color "#646464")
@@ -83,7 +83,7 @@
  '(linum-format " %7i ")
  '(org-src-block-faces 'nil)
  '(package-selected-packages
-   '(exotica-theme tron-legacy-theme diff-hl helm better-defaults treemacs-icons-dired smartparens keycast counsel flycheck treemacs-projectile projectile treemacs expand-region auto-package-update dashboard company doom-modeline org-bullets which-key try use-package))
+   '(ivy-prescient ivy-rich lsp-ivy all-the-icons-ivy all-the-icons-dired treemacs-all-the-icons mode-icons lsp-ui lsp-mode exotica-theme tron-legacy-theme diff-hl helm better-defaults treemacs-icons-dired smartparens keycast counsel flycheck treemacs-projectile projectile treemacs expand-region auto-package-update dashboard company doom-modeline org-bullets which-key try use-package))
  '(pdf-view-midnight-colors '("#ffffff" . "#100f10"))
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
@@ -116,4 +116,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(all-the-icons-dired-dir-face ((t (:foreground nil))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
